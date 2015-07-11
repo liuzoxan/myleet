843248b3fcafa91b46dc7adb95d63b397f558c26
Leon liu <zxliu@travelsky.com>
2015-07-11 10:47:55 -0400

    [LEET-EASY] Reverse Integer

---
+++ leetcode/include/leetcode/leetcode_fwd.h
@@ -0,0 +1,14 @@
+/*
+ * leetcode_fwd.h
+ *
+ *  Created on: Jul 11, 2015
+ *      Author: zxliu
+ */
+
+#ifndef LEETCODE_FWD_H_
+#define LEETCODE_FWD_H_
+
+#include <stdlib.h>
+#include <assert.h>
+
+#endif /* LEETCODE_FWD_H_ */
