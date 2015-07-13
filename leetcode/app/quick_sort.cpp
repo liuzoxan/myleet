97715bb59bafbf83e2cc80ec71972db37790f60b
zxliu <zxliu@travelsky.com>
2015-07-13 23:00:30 +0800

    [LEET] improved quick sort

--- leetcode/app/quick_sort.cpp
+++ leetcode/app/quick_sort.cpp
@@ -11,34 +11,76 @@
 class Solution
 {
 public:
-    void quickSort(vector<int>& nums)
-    {
-    }
+    static const int CUTOFF = 3;
 
-    void insertSort(vector<int>& nums, int start, int end)
+    static void insertSort(int nums[], int left, int right)
     {
-        for (int curr = start + 1; curr < end + 1; curr++)
+        for (int forward = 1; forward <= right; forward++)
         {
-            int saved = nums[curr];
-            int back = curr;
-            for (; back > start and nums[back - 1] > saved; back--)
-                nums[back] = nums[back - 1];
-            nums[back] = saved;
+            int saved = nums[forward];
+            int backward = forward;
+            while(backward > left and nums[backward - 1] > nums[forward])
+            {
+                nums[backward] = nums[backward - 1];
+                backward--;
+            }
         }
     }
+
+    static int median(int nums[], int left, int right)
+    {
+        int med = (left + right) / 2;
+
+        if (nums[left] > nums[med])
+            std::swap(nums[left], nums[med]);
+
+        if (nums[left] > nums[right])
+            std::swap(nums[left], nums[right]);
+
+        if (nums[med] > nums[right])
+            std::swap(nums[med], nums[right]);
+
+        std::swap(nums[med], nums[right - 1]);
+        return nums[right - 1];
+    }
+
+    static void quickSort(int nums[], int left, int right)
+    {
+        int forward = left;
+        int backward = right - 1;
+
+        if (left + CUTOFF <= right)
+        {
+            int medNum = median(nums, left, right);
+
+            for ( ; ; )
+            {
+                while(nums[++forward] < medNum){};
+                while(nums[--backward] > medNum){};
+                if (forward < backward)
+                    std::swap(nums[forward], nums[backward]);
+                else
+                    break;
+            }
+            std::swap(nums[forward], nums[right - 1]);
+            quickSort(nums, left, forward - 1);
+            quickSort(nums, forward + 1, right);
+        }
+        else
+            insertSort(nums, left, right);
+    }
+
 };
 
 int main(int argc, char** argv)
 {
-    int nums[] = {5, 2, 4, 3, 6, 1};
-    vector<int> vint(nums, nums + sizeof(nums)/sizeof(int));
+    int nums[] = {5, 2, 4, 3, 3, 2, 6, 9, 8, 7};
 
-    Solution s;
-    s.insertSort(vint, 0, 5);
+    int size = sizeof(nums)/ sizeof(int);
+    Solution::quickSort(nums, 0, size);
 
-    for (vector<int>::iterator iter = vint.begin(); iter != vint.end(); iter++)
-        cout << *iter << " ";
-
+    for(int i = 0; i < size; i++)
+        cout << nums[i] << ' ';
     cout << endl;
     return EXIT_SUCCESS;
 }
